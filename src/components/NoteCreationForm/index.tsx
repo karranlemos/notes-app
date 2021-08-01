e9d9da7a551ba7cfc8d28eb7633ea0eb69a00fc7
Karran Lemos <karranlemos@gmail.com>
2021-08-01 19:34:17 -0300

    Cria handlers para mudanças no formulário

--- src/components/NoteCreationForm/index.tsx
+++ src/components/NoteCreationForm/index.tsx
@@ -1,16 +1,32 @@
 import { Input, Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState, ChangeEventHandler, MouseEventHandler } from 'react';
 
 import useStyles from './styles';
 
 const NoteCreationForm: React.FC = () => {
   const classes = useStyles();
 
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const titleOnChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const descriptionOnChangeHandler: ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const submitHandler: MouseEventHandler<HTMLButtonElement> = () => {
+    console.log(title, description);
+  };
+
   return (
     <div className={classes.noteCreationForm}>
       <Input
         placeholder="Assunto"
         disableUnderline
+        onChange={titleOnChangeHandler}
         classes={{
           root: classes.title,
         }}
@@ -19,12 +35,14 @@ const NoteCreationForm: React.FC = () => {
         placeholder="Texto"
         disableUnderline
         multiline
+        onChange={descriptionOnChangeHandler}
         classes={{
           root: classes.description,
         }}
       />
       <Button
         variant="contained"
+        onClick={submitHandler}
         classes={{
           root: classes.button,
         }}
