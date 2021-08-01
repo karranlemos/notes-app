f280e6aaef27c33b288cb54c58b58735b5a9b701
Karran Lemos <karranlemos@gmail.com>
2021-08-01 17:13:04 -0300

    Configura eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      Hey
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div>
+    Hey
+  </div>
+);
 
 export default App;
